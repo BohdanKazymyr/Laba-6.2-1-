b8ccb5960ea3ab6e77026dfeb0c5eb4eeb05588c
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-11-10 15:25:14 +0200

    f

---
+++ Laba 6.2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int Create(int* a, const int size, int Low, int High)
+{
+	double i = 0;
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+	return 0;
+}
+int Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << a[i];
+	cout << endl;
+	return 0;
+}
+int Sum(const int size, int a[])
+	{
+		int i;
+		int sum = 0;
+		for (i = 0; i < size; i++)
+			if (i % 2 == 0)
+				sum = sum + a[i];
+			else
+				sum += 0;
+		return sum*1.0 / 10.0;
+	}
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 21;
+	int i = 0;
+	int a[n];
+	int Low = 15;
+	int High = 85;
+	Create(a, n, Low, High);
+	Print(a, n);
+	cout << "S = " << Sum(n, a) << endl;
+	
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 6.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{   
+			int n = 21;
+			int i = 0;
+			int a[n];
+			int Low = 15;
+			int High = 85;
+			Create(a, n, Low, High);
+			Print(a, n);
+			Sum(n, a);
+		}
+	};
+}
